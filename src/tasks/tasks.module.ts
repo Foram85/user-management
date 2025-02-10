3389e2c28b8886bcc5d0fa1b9ec711d1884cc108
Foram-SoluteLabs <foram.s@solutelabs.com>
2025-02-10 13:03:16 +0530

    Update tasks.module.ts

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,14 +1,13 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
-import { TasksRepository } from './tasks.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Tasks } from './entities/tasks.entity';
 import { UsersModule } from 'src/users/users.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Tasks]), forwardRef(() => UsersModule)],
-  providers: [TasksService, TasksRepository],
+  providers: [TasksService],
   controllers: [TasksController],
 })
 export class TasksModule {}
